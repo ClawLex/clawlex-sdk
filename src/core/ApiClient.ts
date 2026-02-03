8b1d7fdc062d55c486ed929f319f2189e3fb6c99
ClawLex <karanfan47@gmail.com>
2026-02-03 11:05:00 +0530

    feat(sdk): implement ClawLexError base and custom error types

--- src/core/ApiClient.ts
+++ src/core/ApiClient.ts
@@ -1,2 +1,4 @@
 import axios, { AxiosInstance, AxiosRequestConfig } from 'axios';
 import { ClawLexConfig } from '../types';
+
+export class ClawLexError extends Error { constructor(m:string){super(m); Object.setPrototypeOf(this, ClawLexError.prototype);} }
