1f1c7e6004f7c35e0d23fa5245be5a3a2c18dd00
ClawLex <karanfan47@gmail.com>
2026-02-06 15:57:00 +0530

    feat(sdk): deploy ApiClient constructor and request initialization

--- src/core/ApiClient.ts
+++ src/core/ApiClient.ts
@@ -2,3 +2,8 @@ import axios, { AxiosInstance, AxiosRequestConfig } from 'axios';
 import { ClawLexConfig } from '../types';
 
 export class ClawLexError extends Error { constructor(m:string){super(m); Object.setPrototypeOf(this, ClawLexError.prototype);} }
+
+export class ApiClient {
+  constructor(private config: ClawLexConfig) {}
+  public async get<T>(u:string):Promise<T>{ return (await axios.get(u)).data; }
+}
